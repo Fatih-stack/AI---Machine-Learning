f2090b96b4110a85b91d9ca215e14fe1509c7ba2
fatih <fdurmaz1905@gmail.com>
2023-06-26 14:54:57 +0300

    Test population

---
+++ Lesson10/test_population.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 26 00:17:05 2023
+
+@author: Fatih Durmaz
+test population is distributed normally based on sample
+"Kolmogorov-Smirnov" test and "Shapiro-Wilk" test
+if p value is bigger than 0.05 -> normal  dist 
+"""
+
+from scipy.stats import uniform, shapiro, kstest, norm
+
+x = uniform.rvs(size=1000)
+
+stat, pval = kstest(x, 'norm')
+
+print(stat, pval)  # 0.5003124945817926 5.217793307475998e-232
+
+import matplotlib.pyplot as plt 
+
+plt.hist(x)
+plt.show()
+
+a = norm.rvs(size=1000)
+plt.hist(a)
+plt.show()
+stat, pval = kstest(a, 'norm')
+print(f'p değeri(norm, mean:0, sd:1): {pval}')
+
+# mean -> 100, stddev -> 15
+a = norm.rvs(100, 15, size=1000)
+plt.hist(a)
+plt.show()
+stat, pval = kstest(a, cdf=norm.cdf, args=(100, 15))
+print(f'p değeri(norm, mean:100, sd:15): {pval}')
+
+print('Shapiro-Wilk test with mean:100 sd:15')
+a = norm.rvs(100, 15, size=100)
+plt.hist(a)
+plt.show()
+stat, pval = shapiro(a)
+print(f'p değeri(norm-shapiro): {pval}')
+
+a = uniform.rvs(100, 15, size=100)
+plt.hist(a)
+plt.show()
+stat, pval = shapiro(a)
+print(f'p değeri(uniform-shapiro): {pval}')
